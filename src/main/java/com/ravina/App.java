ef39088b08fd6d4d45745f9f614e50a31452ba52
ravina <t.ravinacbe@gmail.com>
2021-11-05 00:20:47 +0530

    first commit

--- src/main/java/com/ravina/App.java
+++ src/main/java/com/ravina/App.java
@@ -1,3 +1,4 @@
+
 package com.ravina;
 
 import java.util.List;
@@ -15,52 +16,27 @@ import org.hibernate.service.ServiceRegistryBuilder;
  */
 public class App {
 	public static void main(String[] args) {
-		Stud s = new Stud();
-		s.setRollno(1000);
-		s.setName("Muruga");
-		s.setMarks(98);
 
-		Stud s1 = new Stud();
-		s1.setRollno(2000);
-		s1.setName("vinayaga");
-		s1.setMarks(99);
-
-		Stud s2 = new Stud();
-		s2.setRollno(3000);
-		s2.setName("soliyayi");
-		s2.setMarks(100);
-
+		Stud s3 = new Stud();
 		Configuration c = new Configuration().configure().addAnnotatedClass(Stud.class);
 		ServiceRegistry sr = new ServiceRegistryBuilder().applySettings(c.getProperties()).buildServiceRegistry();
 		SessionFactory sessionFactory = c.buildSessionFactory(sr);
 		Session session = sessionFactory.openSession();
 		Transaction tx = session.beginTransaction();
-		session.save(s);
-		session.save(s1);
-		session.save(s2);
+		s3 = (Stud) session.get(Stud.class, 3000);
 		tx.commit();
-		//another way to save objects in session
-		//session.beginTransaction();
-		//session.save(s); //Saving the first UserInfoobject
-		//session.save(s1); //Saving the second UserInfoobject
-		//session.save(s2); 
-		//session.getTransaction().commit();
+		System.out.println(s3);
 
-		
-
-		//Retrieving all the records of UserInfo
+		// Retrieving all the records of UserInfo
 		Query query1 = session.createQuery("from Stud");
-		List<Stud> collection1 =  query1.list();
+		List<Stud> collection1 = query1.list();
 		System.out.println("Retrieving and displaying details of all the users");
-		for(Stud object : collection1)
-		{
+		for (Stud object : collection1) {
 			System.out.println("Id : " + object.getRollno());
 			System.out.println("First Name : " + object.getName());
-			System.out.println("Last Name : " + object.getMarks());	
+			System.out.println("Last Name : " + object.getMarks());
 			System.out.println();
 		}
 
 	}
 }
-
-
